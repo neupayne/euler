90f92cc3395aee152fc90bbaa967cbe3e49a1f53
neupayne <dnzitu06@gmail.com>
2016-04-03 17:27:38 +0300

    test

--- euler_3.py
+++ euler_3.py
@@ -3,7 +3,7 @@ def the_flying_circus():
     if 1<5:    # Start coding here!
         return True    # Don't forget to indent
         # the code inside this block!
-    elif 6>8 and 9 !=10
+    elif 6>8 and 9 !=8
         return False
     else :
         return False
